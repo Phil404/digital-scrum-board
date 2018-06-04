91fdfff0ec344300885b697d2932460c33ff772a
Mathis Becker <moin@mathisbecker.de>
2018-06-04 12:19:50 +0200

    Spring Setup

---
+++ src/main/java/de/utini/digitalscrumboard/DigitalScrumBoardApplication.java
@@ -0,0 +1,12 @@
+package de.utini.digitalscrumboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalScrumBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalScrumBoardApplication.class, args);
+    }
+}
