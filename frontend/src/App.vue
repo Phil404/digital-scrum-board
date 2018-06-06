92c97d634dba4267a0249732815df24029759f6e
mbecker6 <moin@mathisbecker.de>
2018-06-06 18:29:49 +0200

    update .gitignore to ignore logfiles

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,60 +1,155 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div id="board">
+        <b-tabs>
+            <b-tab v-for="board in dumyBoard" :key="board.id" :title="board.name" active>
+                <b-container fluid class="">
+                    <b-row>
+                        <b-col></b-col>
+                        <b-col v-for="state in board.states" :key="state.id">{{ state.name }}</b-col>
+                    </b-row>
+                    <b-row v-for="story in board.stories" :key="story.id">
+                        <b-col>{{ story.title }}</b-col>
+                        <b-col v-for="state in board.states" :key="state.id">
+                            <p v-for="task in story.tasks">{{insertTask(state,task)}}</p>
+                        </b-col>
+                    </b-row>
+                </b-container>
+            </b-tab>
+        </b-tabs>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    export default {
+        name: 'app',
+        data() {
+            return {
+                dumyBoard:
+                    [
+                        {
+                            id: 1,
+                            name: "superboard",
+                            states: [
+                                {
+                                    id: 1,
+                                    name: "in Progress"
+                                },
+                                {
+                                    id: 2,
+                                    name: "QA"
+                                },
+                                {
+                                    id: 3,
+                                    name: "Done"
+                                }
+                            ],
+                            stories: [
+                                {
+                                    id: 1,
+                                    title: "story1",
+                                    description: "do shit faster",
+                                    startDate: 23 - 12 - 1993,
+                                    creatDate: 24 - 12 - 1993,
+                                    doneDate: 25 - 12 - 1993,
+                                    tasks: [
+                                        {
+                                            id: 1,
+                                            state: 1,
+                                            assignee: [1, 2],
+                                            title: "task #1",
+                                            description: "we need to do some stuff",
+                                            startDate: 23 - 12 - 1994,
+                                            creatDate: 24 - 12 - 1994,
+                                            doneDate: 25 - 12 - 1994
+                                        },
+                                        {
+                                            id: 3,
+                                            state: 3,
+                                            assignee: [1, 2],
+                                            title: "task #1",
+                                            description: "we need to do some stuff",
+                                            startDate: 23 - 12 - 1994,
+                                            creatDate: 24 - 12 - 1994,
+                                            doneDate: 25 - 12 - 1994
+                                        }
+                                    ]
+                                },
+                                {
+                                    id: 3,
+                                    title: "story2",
+                                    description: "do stuff faster",
+                                    startDate: 12 - 12 - 1993,
+                                    creatDate: 13 - 12 - 1993,
+                                    doneDate: 14 - 12 - 1993,
+                                    tasks: [
+                                        {
+                                            id: 2,
+                                            state: 2,
+                                            assignee: [1, 2],
+                                            title: "Dummytask",
+                                            description: "fix bug",
+                                            startDate: 23 - 12 - 1994,
+                                            creatDate: 24 - 12 - 1994,
+                                            doneDate: 25 - 12 - 1994
+                                        }
+                                    ]
+                                }
+
+                            ]
+                        },
+                        {
+                            id: 2,
+                            rows: [1, 2, 3],
+                            columns: [7, 6, 5],
+                            name: "überboard"
+                        }
+                    ]
+            }
+        },
+        methods: {
+            insertTask: function (state, task) {
+                if (state.id === task.state) {
+                    return task
+                }
+                return null;
+            }
+        },
+        mounted() {
+
+        }
+
     }
-  }
-}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 
-h1, h2 {
-  font-weight: normal;
-}
+    h1, h2 {
+        font-weight: normal;
+    }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
 
-a {
-  color: #42b983;
-}
+    a {
+        color: #42b983;
+    }
+
+    .grid {
+        border: 1px solid grey;
+    }
 </style>
