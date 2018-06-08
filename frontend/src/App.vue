2b818edf1a4c5c2ac57fe44479edbded9cac2d12
mbecker6 <moin@mathisbecker.de>
2018-06-08 09:18:59 +0200

    add several modals

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -11,14 +11,15 @@
                         <b-col :cols="storyTitleCols">
                             <b>{{ story.title }}</b>
                             <br>
-                            {{ story.description }}<br>
-                            <b-btn v-on:click="" size="sm" variant="primary"> Add Task</b-btn>
+                            <b-btn v-on:click="showAddTaskModal(story, board)" size="sm" variant="primary"> Add Task
+                            </b-btn>
                             <b-btn v-on:click="showStoryModal(board, story)" size="sm" variant="secondary">Info</b-btn>
                         </b-col>
                         <b-col v-for="state in board.states" :key="state.id">
-                            <b-card v-show="insertTask(state,task)" :title="task.title" v-for="task in story.tasks"
+                            <b-card class="taskCard" v-show="insertTask(state,task)" :title="task.title"
+                                    v-for="task in story.tasks"
                                     :key="task.id">
-                                <div v-on:click="showTaskModal(board, task)">
+                                <div class="taskCardText" v-on:click="showTaskModal(board, task)">
                                     {{insertTask(state,task)}}
                                 </div>
                                 <br>
@@ -35,7 +36,7 @@
         <b-button class="addStoryButton" size="sm" variant="primary" >Add Story</b-button>
 
 
-        <b-modal  size="lg"v-bind="taskModalDataContainer" ref="taskModal" :title="taskModalDataContainer.title">
+        <b-modal size="lg" v-bind="taskModalDataContainer" ref="taskModal" :title="taskModalDataContainer.title">
             <b-container>
                 <b-row>
                     <b-col>
@@ -59,17 +60,44 @@
 
         <b-modal size="lg" v-bind="storyModalDataContainer" ref="storyModal" :title="storyModalDataContainer.title">
             <b-container>
-                <b-col>
-                    <b>Description</b>
-                    <br>
-                    {{storyModalDataContainer.description}}
-                </b-col>
-                <b-col cols="4">
-                    <b>Start Date</b><br>{{ storyModalDataContainer.startDate}}
-                </b-col>
+                <b-row>
+                    <b-col>
+                        <b>Description</b>
+                        <br>
+                        {{storyModalDataContainer.description}}
+                    </b-col>
+                </b-row>
+                <hr>
+                <b-row>
+                    <b-col>
+                        <b>Start Date</b><br>{{ storyModalDataContainer.startDate}}
+                    </b-col>
+                    <b-col>
+                        <b>Create Date</b><br>{{ storyModalDataContainer.startDate}}
+                    </b-col>
+                    <b-col>
+                        <b>Done Date</b><br>{{ storyModalDataContainer.startDate}}
+                    </b-col>
+                </b-row>
             </b-container>
             <b-container slot="modal-footer">
-                <b-button size="sm" variant="primary" v-on:click="hideTaskModal">Close</b-button>
+            </b-container>
+        </b-modal>
+
+
+        <b-modal size="lg" v-bind="addTaskDataContainer" ref="addTaskModal" title="Add a Task">
+            <b-container>
+                <div>
+                    <b-form-input v-model="addTaskDataContainer.taskName" type="text" placeholder="Task Name"></b-form-input>
+                    <b-form-textarea v-model="addTaskDataContainer.description" type="text" placeholder="Task Description" :rows="3"
+                                     :max-rows="6">>
+                    </b-form-textarea>
+                    <b-form-select v-model="addTaskDataContainer.assignee" :options="addTaskDataContainer.users"
+                                   class="mb-3"></b-form-select>
+                </div>
+            </b-container>
+            <b-container slot="modal-footer">
+                <b-button v-on:click="addTask()" variant="primary">Add to Story</b-button>
             </b-container>
         </b-modal>
     </div>
@@ -86,10 +114,22 @@
                 storyTitleCols: 1,
                 taskModalDataContainer: {},
                 storyModalDataContainer: {},
+                addTaskDataContainer: {},
                 //UNTIL HERE
 
-                users: [],
-                boards: [],
+                users: [
+                    {
+                        id: 1,
+                        name: "John Doe",
+                        role: "Developer"
+                    },
+                    {
+                        id: 2,
+                        name: "Aria Stark",
+                        role: "QA"
+                    }
+
+                ],
                 dummyBoard:
                     [
                         {
@@ -107,6 +147,10 @@
                                 {
                                     id: 3,
                                     name: "Done"
+                                },
+                                {
+                                    id: 4,
+                                    name: "Live"
                                 }
                             ],
                             stories: [
@@ -123,7 +167,7 @@
                                             state: 1,
                                             assignees: [1, 2],
                                             title: "task #1",
-                                            description: "we need to do some stuff",
+                                            description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.",
                                             startDate: "23 - 12 - 1994",
                                             creatDate: "24 - 12 - 1994",
                                             doneDate: "25 - 12 - 1994"
@@ -133,7 +177,7 @@
                                             state: 3,
                                             assignees: [1, 2],
                                             title: "task #1",
-                                            description: "we need to do some stuff",
+                                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus consectetur facilisis eleifend. Sed neque tortor, condimentum non scelerisque feugiat, iaculis ut lectus.",
                                             startDate: "23 - 12 - 1994",
                                             creatDate: "24 - 12 - 1994",
                                             doneDate: "25 - 12 - 1994"
@@ -153,7 +197,7 @@
                                             state: 2,
                                             assignees: [1, 2],
                                             title: "Dummytask",
-                                            description: "fix bug",
+                                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus consectetur facilisis eleifend.",
                                             startDate: "23 - 12 - 1994",
                                             creatDate: "24 - 12 - 1994",
                                             doneDate: "25 - 12 - 1994"
@@ -172,57 +216,7 @@
                     ]
             }
         },
-        mounted() {
-            this.$http.get("http://localhost:8080/rest/users").then(response => {
-                this.users = response.body;
-            }, error => {
-                console.error(error);
-            });
-
-            this.collectData();
-        },
         methods: {
-            collectData: function () {
-                this.$http.get("http://localhost:8080/rest/boards").then(response => {
-                    this.boards = response.body;
-
-                    this.boards.forEach(
-                        board => {
-                            this.$http.get("http://localhost:8080/rest/boards/" + board.id + "/states").then(
-                                response => {
-                                    board.states = response.body;
-                                },
-                                error => {
-                                    console.error(error);
-                                }
-                            );
-
-                            this.$http.get("http://localhost:8080/rest/boards/" + board.id + "/stories").then(
-                                response => {
-                                    board.stories = response.body;
-
-                                    board.stories.forEach(
-                                        story => {
-                                            this.$http.get("http://localhost:8080/rest/boards/" + board.id + "/stories/" + story.id + "/tasks").then(
-                                                response => { story.tasks = response.body; },
-                                                error => { console.error(error); }
-                                            )
-                                        }
-                                    )
-                                },
-                                error => {
-                                    console.error(error);
-                                }
-                            )
-                        }
-                    );
-
-                    console.log(this.boards);
-                    console.log(this.dummyBoard);
-                }, error => {
-                    console.error(error);
-                });
-            },
             insertTask: function (state, task) {
                 if (state.id === task.state) {
                     return task.description
@@ -238,7 +232,7 @@
                 let users = this.users;
                 console.log(task.assignees);
                 let assignees = [];
-                taskCopy.assignees.forEach(function (assignee) {
+                task.assignees.forEach(function (assignee) {
                     users.forEach(function (user) {
                         if (assignee === user.id) {
                             assignees.push(user);
@@ -253,6 +247,18 @@
                 this.storyModalDataContainer = story;
                 this.$refs.storyModal.show();
             },
+            showAddTaskModal: function (story, board) {
+                this.addTaskDataContainer.story = story;
+                this.addTaskDataContainer.board = board;
+                let userNames = [];
+                this.users.forEach(function (user) {
+                    let nameObj = {value: user.name, text: user.name};
+                    userNames.push(nameObj)
+                });
+                console.log(userNames);
+                this.addTaskDataContainer.users = userNames;
+                this.$refs.addTaskModal.show();
+            },
             moveTaskRight: function (board, task) {
                 if (task.state < board.states.length) {
                     task.state += 1;
@@ -265,6 +271,19 @@
                     return false;
                 }
             },
+            addTask() {
+                let task = {
+                    id: null,
+                    state: 1,
+                    assignees: this.addTaskDataContainer.assignee,
+                    title: this.addTaskDataContainer.taskName,
+                    description: this.addTaskDataContainer.description,
+                    startDate: "23 - 12 - 1994",
+                    creatDate: "24 - 12 - 1994",
+                    doneDate: "25 - 12 - 1994"
+                };
+                console.log(task)
+            }
         },
         computed: {}
     }
@@ -298,12 +317,16 @@
         color: #42b983;
     }
 
-    taskCard:hover {
-        background-color: lightgrey;
-        cursor: pointer;
+    .taskCard:hover {
+        background-color: #b3b3b3;
     }
 
-    addStoryButtn {
+    .taskCardText:hover {
+        cursor: pointer;
+        color: whitesmoke;
+    }
+
+    .addStoryButtn {
         position: fixed;
         bottom: 0;
         left: 0;
